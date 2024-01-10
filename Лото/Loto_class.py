7a8dfdf8c93e615049f985745623aa27c96d15e2
Aksey010 <143290412+Aksey010@users.noreply.github.com>
2024-01-10 16:04:54 +0300

    Add files via upload

---
+++ Лото/Loto_class.py
@@ -0,0 +1,78 @@
+import random
+
+
+class Loto:
+    def __init__(self):
+        self.card = ''
+        self.current_number = None
+        self.numbers_in_game = list(range(1, 91))
+        self.card_number = None
+        self.player_name = ''
+        self.bot = None
+
+    def refill_numbers_in_game(self):
+        self.numbers_in_game = list(range(1, 91))
+
+    def get_random_number(self):
+        self.current_number = random.sample(self.numbers_in_game, 1)
+        self.numbers_in_game.remove(self.current_number[0])
+
+    def new_card_number(self):
+        self.card_number = random.sample(range(1, 91), 15)
+
+    def compute_card(self):
+
+        a = self.card_number[:5]
+        b = self.card_number[5:10]
+        c = self.card_number[10:15]
+
+        for i in range(len(a)):
+            if a[i] < 10:
+                a[i] = str(a[i])
+                a[i] = a[i] + ' '
+
+        for i in range(len(b)):
+            if b[i] < 10:
+                b[i] = str(b[i])
+                b[i] = b[i] + ' '
+
+        for i in range(len(c)):
+            if c[i] < 10:
+                c[i] = str(c[i])
+                c[i] = c[i] + ' '
+
+        _ = random.sample(range(0, 6), 3)
+        for i in _:
+            a.insert(i, '  ')
+
+        _ = random.sample(range(0, 6), 3)
+        for i in _:
+            b.insert(i, '  ')
+
+        _ = random.sample(range(0, 6), 3)
+        for i in _:
+            c.insert(i, '  ')
+
+        a = ' ' + str(a).replace(',', '').replace("'", "").replace('[', '').replace(']', '')
+        b = ' ' + str(b).replace(',', '').replace("'", "").replace('[', '').replace(']', '')
+        c = ' ' + str(c).replace(',', '').replace("'", "").replace('[', '').replace(']', '')
+
+        d = a + '\n' + b + '\n' + c
+
+        self.card = d
+
+    def show_card(self, name=''):
+        if name == '':
+            print(25*'-')
+            print(self.card)
+            print(25*'-')
+        else:
+            print(name.center(25, '-'))
+            print(self.card)
+            print(25*'-')
+
+    def pass_in_card(self):
+        if self.current_number[0] > 9:
+            self.card = self.card.replace(' ' + str(self.current_number[0]), ' --')
+        else:
+            self.card = self.card.replace(' ' + str(self.current_number[0]) + ' ', ' --')
---
+++ Лото/Игра лото.py
@@ -0,0 +1,91 @@
+from projects.Project.Лото.Loto_class import Loto
+
+# Сама игра
+while True:
+    mark_list = []                                                                                                      # Список с индексами игроков на исключение
+    players_list = []                                                                                                   # Список игроков в игре
+    winners_list = []                                                                                                   # Список победителей
+    loto_number = Loto()                                                                                                # Числа будут генерироваться из этой переменной дял всех
+    a = 0                                                                                                               # Количество игроков
+    t_f = ''                                                                                                            # Бот или не бот
+    y_n = ''                                                                                                            # Зачеркнуть число или нет
+    repeat = ''                                                                                                         # Продолжить игру или нет
+    # Инициализация игроков
+    while a <= 0:
+        try:
+            a = int(input('Введите количество игроков: '))
+        except:
+            print(f'Ошибка. Введите количество игроков ещё раз')
+
+    for i in range(a):
+        players_list.append(exec("player_{} = {}".format(i, i)))
+
+    # Для каждого игрока создаётся имя, числа в карте и сама карта
+    for i in range(len(players_list)):
+        players_list[i] = Loto()
+        players_list[i].player_name = input(f"Введите имя игрока_{i}: ")
+        while t_f != 't' and t_f != 'f':
+            t_f = input("Введите, является ли игрок ботом (t, f): ")
+        if t_f == 't':
+            players_list[i].bot = True
+        elif t_f == 'f':
+            players_list[i].bot = False
+
+        t_f = ''                                                                                                        # Сброс ответа, чтобы спрашивать каждого игрока
+
+        players_list[i].new_card_number()
+        players_list[i].compute_card()
+
+    # Приступаем к игровой сессии
+    while True:
+        mark_list = []                                                                                                  # Обнуляем список каждый раз
+        loto_number.get_random_number()                                                                                 # Получаем одно число на всех
+        print(f"Новый бочонок: {loto_number.current_number} (осталось {len(loto_number.numbers_in_game)})")
+        for i in players_list:                                                                                          # Показываем карты всех игроков
+            i.show_card(i.player_name)
+            i.current_number = loto_number.current_number                                                               # Передаём одно число всем
+
+        for i in players_list:
+            if i.bot:
+                i.pass_in_card()
+                i.card_number.remove(loto_number.current_number[0])
+            else:
+                y_n = ''                                                                                                # Сброс ответа, чтобы спрашивать каждого игрока
+                while y_n != 'y' and y_n != 'n':
+                    y_n = input(f"Игрок {i.player_name}: Зачеркнуть цифру? (y/n): ")
+
+                if y_n == 'y' and loto_number.current_number[0] in i.card_number:
+                    i.pass_in_card()
+                    i.card_number.remove(loto_number.current_number[0])
+                elif y_n == 'n' and loto_number.current_number[0] not in i.card_number:
+                    continue
+                else:
+                    print(f"Игрок {i.player_name} проиграл")
+                    mark_list.append(i)
+
+                if not i.card_number:
+                    print(f'Игрок {i.player_name} закончил')
+                    winners_list.append(i)
+                    players_list.remove(i)
+
+        for i in range(len(mark_list)):
+            players_list.remove(mark_list[i])
+
+        if not players_list:
+            print('*' * 50)
+            break
+
+        print('*' * 50)                                                                                                 # Разделение раундов
+
+        for i in range(len(winners_list)):
+            print(f'Игрок {winners_list[i].player_name} занял {i + 1} место')
+
+        if not winners_list:
+            print('Все проиграли. Удачи в следующий раз!')
+
+    while repeat != 'y' and repeat != 'n':
+        repeat = input(f'Хотите сыграть ещё раз? (y,n) ')
+    if repeat == 'y':
+        continue
+    else:
+        break
